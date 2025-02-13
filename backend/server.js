27d14e4d9f88881d92d84ba5dca8d81b6cd62f5d
LLLLLrf <1626226046@qq.com>
2025-02-13 19:36:13 +0800

    server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import audioRoutes from './routes/audioRoutes.js';
 import videoRoutes from './routes/videoRoutes.js';
+import fs from 'fs'
+
+const path = './app.log';
 // import getImage from './routes/getImage.js';
 
 const app = express();
@@ -12,12 +15,22 @@ app.use('/api/video', videoRoutes);
 app.use('/static', express.static('assets'));
 
 
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
 // 全局错误处理
 app.use((err, req, res, next) => {
   console.error('Unhandled Error:', err.message);
   res.status(500).json({ error: err.message || 'Internal Server Error' });
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+
+app.listen(port, '0.0.0.0', (err) => {
+  if (err) {
+      fs.appendFileSync(path, `Error: ${err.message}\n`);
+      console.error('Error starting server:', err);
+  } else {
+      console.log(`Server running on http://0.0.0.0:${port}`);
+  }
 });
--- frontend/utils/config.js
+++ frontend/utils/config.js
@@ -1,7 +1,9 @@
 
 const config = {
+  // 局域网
   serverUrl: 'http://192.168.43.12:3000',
-  // serverUrl: 'http://219.223.196.227:3000',
+
+  // serverUrl: 'http://113.106.163.66:3000',
 };
 
 module.exports = config;
